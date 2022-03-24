5dfeddfd711971f669aeebef1db896305dc20906
msahinnnn <mehemt1907cr7@gmail.com>
2022-03-24 21:16:23 +0300

    Add project files.

---
+++ Accountant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_OCP
+{
+    internal class Accountant : IPersonel
+    {
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Salary { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+        public void CalculateCost()
+        {
+            Console.WriteLine("Accountant : Calculate cost...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SOLID_OCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let's say we add a new job position called Accountant.
+            //It is necessary to add new code without touching the existing code.
+        }
+    }
+}
